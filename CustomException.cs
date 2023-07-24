b505564d90b84998a20f47771585532841e97e25
sas03 <stephanesumo@yahoo.fr>
2023-07-24 12:49:33 +0200

    Exceptions

---
+++ CustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class CustomException : Exception
+    {
+        public string Message { get; set; }
+        public DateTime DateTime { get; set; }
+        public CustomException(string message) : base(message)
+        {
+            Message = message;
+            DateTime = DateTime.Now;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Exceptions;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            throwError(7);
+        }
+        catch(CustomException e)
+        {
+            Console.WriteLine(e.Message + " : " + e.DateTime);
+        }
+    }
+
+    public static void throwError(int value)
+    {
+        if(value > 5)
+        {
+            throw new CustomException($"Number typed {value} can't be greater than 5");
+        }
+    }
+}
